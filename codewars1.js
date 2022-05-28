a7736d971b1dd73614c99be18d0ee5c2b316a566
Oluwadamilola Ajayi <70576110+dodomiyake@users.noreply.github.com>
2022-05-28 09:32:55 +0100

    Sum of two lowest positive integers

--- codewars1.js
+++ codewars1.js
@@ -1,4 +1,4 @@
-function sumTwoSmallestNumbers(numbers) { 
+function sumTwoSmallestNumbers(numbers) {
   let sortNum = numbers.sort(function(a, b){return a - b})
   return sortNum[0] + sortNum[1]
 }
